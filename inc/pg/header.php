5a3cf9ba4a1dd8a8251cc76e2be54dda3f886f0e
Josilv3296 <54422217+josilv3296@users.noreply.github.com>
2024-03-13 19:57:37 +0000

    add init header structure

---
+++ inc/pg/header.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <title></title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="author" content="Josilv">
+  <meta name="keywords" content="">
+  <meta name="description" content="">
+  <!-- FAVICON -->
+  <link rel="icon" type="image/png" sizes="32x32" href="./inc/img/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="./inc/img/favicon-16x16.png">
+  <!-- Main CSS Style -->
+  <link rel="stylesheet/less" href="./inc/css/main.css" />
+  <!-- Bootstrap CSS v5.2.0-beta1 -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+</head>
+
+<body>
