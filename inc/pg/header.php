966715a75f401dfafcb53382e0d7f87cc94f0f25
Josilv3296 <54422217+josilv3296@users.noreply.github.com>
2024-03-13 20:27:55 +0000

    Fix header & Setup configuration options
    Adjust code for the new changes

--- inc/pg/header.php
+++ inc/pg/header.php
@@ -1,19 +1,18 @@
 <!doctype html>
-<html lang="en">
+<html lang="<?php echo WB_LANG; ?>">
 
 <head>
-  <title></title>
+  <title><?php echo WB_TITLE; ?></title>
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="author" content="Josilv">
-  <meta name="keywords" content="">
-  <meta name="description" content="">
+  <meta name="author" content="<?php echo WB_AUTHOR; ?>">
+  <meta name="keywords" content="<?php echo WB_TAGS; ?>">
+  <meta name="description" content="<?php echo WB_DESC; ?>">
   <!-- FAVICON -->
-  <link rel="icon" type="image/png" sizes="32x32" href="./inc/img/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="./inc/img/favicon-16x16.png">
+  <link rel="icon" type="image/png" href="./inc/img/favicon-96x96.png">
   <!-- Main CSS Style -->
-  <link rel="stylesheet/less" href="./inc/css/main.css" />
+  <link rel="stylesheet" href="./inc/css/main.css" />
   <!-- Bootstrap CSS v5.2.0-beta1 -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 </head>
